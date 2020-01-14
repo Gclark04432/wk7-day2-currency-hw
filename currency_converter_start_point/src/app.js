2c90722b03ca1c5453ef751c16c0f0af1fddbdc7
Gary Clark <gclark04432@live.co.uk>
2020-01-14 17:17:07 +0000

    set up funcrtion body

--- currency_converter_start_point/src/app.js
+++ currency_converter_start_point/src/app.js
@@ -4,10 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      currencies: [],
+      selectedCurrency: null
+    },
+    mounted() {
+      this.populateCurrencies()
     },
     methods: {
-
+      populateCurrencies: function () {
+        fetch('https://api.exchangeratesapi.io/latest')
+          .then(receivedCurrencies => receivedCurrencies.json())
+          .then(data => this.currencies = data.rates)
+          .then(console.log(this.currencies))
+      }
     }
   });
 });
